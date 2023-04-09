621f976d0671afcb8e14e04a78aa1452f4247509
Arya Kamandanu <1829141024@student.unm.ac.id>
2023-04-10 00:02:25 +0800

    starter kit 2.1

--- 7._DHT11/7._DHT11.ino
+++ 7._DHT11/7._DHT11.ino
@@ -1,58 +1,50 @@
-#include "DHT.h" //library sensor yang telah diimportkan
+#define BLYNK_PRINT Serial
+
+#define BLYNK_AUTH_TOKEN "oRP17_S_QER-8ZwKHleG1ltf-5W5KHIY"
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+
 #define DHTPIN 5     //Pin apa yang digunakan
 #define DHTTYPE DHT11   // DHT 11
- 
+
+
+char ssid[] = "Tukang Ngoding";
+char pass[] = "kodingeverywhere";
+
 DHT dht(DHTPIN, DHTTYPE);
- 
-void setup() {
-  Serial.begin(115200); //baud komunikasi serial
-  Serial.println("Pengujian DHT11!"); //penulisan di serial monitor
-  dht.begin(); //prosedur memulai pembacaan module sensor
-}
- 
-void loop() {
-  delay(2000);  //menunggu beberapa detik untuk pembacaan
- 
-  //pembacaan sensor membutuhkan waktu 250ms
- 
-  //Pembacaan untuk data kelembaban
-  float humidity_1 = dht.readHumidity();
-  //Pembacaan dalam format celcius (c)
-  float celcius_1 = dht.readTemperature();
-  //pembacaan dalam format Fahrenheit
-  float fahrenheit_1 = dht.readTemperature(true);
- 
-  //mengecek pembacaan apakah terjadi kegagalan atau tidak
-  if (isnan(humidity_1) || isnan(celcius_1) || isnan(fahrenheit_1)) {
-    Serial.println("Pembacaan data dari module sensor gagal!");
+
+BlynkTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
     return;
   }
- 
-  float htof = dht.computeHeatIndex(fahrenheit_1, humidity_1); 
-  //Prosedur pembacaaan data indeks panas dalam bentuk fahreheit
-  float htoc = dht.computeHeatIndex(celcius_1, humidity_1, false);
- //Prosedur pembacaaan data indeks panas dalam bentuk celcius
- 
-  //pembacaan nilai pembacaan data kelembaban
-  Serial.print("Kelembaban: ");
-  Serial.print(humidity_1);
-  Serial.print(" %\t");
-  
-  //pembacaan nilai pembacaan data suhu
-  Serial.print("Suhu : ");
-  Serial.print(celcius_1); //format derajat celcius
-  Serial.print("°"); //simbol derajat
-  Serial.print("C / ");
-  Serial.print(fahrenheit_1); //format derajat fahrenheit
-  Serial.print("°");
-  Serial.print("F\t");
- 
-  Serial.print("Indeks Panas: ");
-  Serial.print(htof); 
-  Serial.print("°");
-  Serial.print("F / ");
-  Serial.print(htoc);
-  Serial.print("°");
-  Serial.println("C ");
- 
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V1, h);
+  Blynk.virtualWrite(V0, t);
+}
+
+void setup() {
+  Serial.begin(115200); //baud komunikasi serial
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  Serial.println("Pengujian DHT11!"); //penulisan di serial monitor
+  dht.begin(); //prosedur memulai pembacaan module sensor
+  timer.setInterval(500L, sendSensor);
+}
+
+void loop() {
+  Blynk.run();
+  timer.run();
 }
--- 8.Sensor_Hujan/8.Sensor_Hujan.ino
+++ 8.Sensor_Hujan/8.Sensor_Hujan.ino
@@ -1,21 +1,48 @@
-const int sensor_hujan = 5;
-const int LED = 4;
+#define BLYNK_PRINT Serial
+
+#define BLYNK_AUTH_TOKEN            "oRP17_S_QER-8ZwKHleG1ltf-5W5KHIY"
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define hujan 5
+#define buzzer 4
+
+char ssid[] = "Tukang Ngoding";
+char pass[] = "kodingeverywhere";
+
+WidgetLED led3(V3);
+
+BlynkTimer timer;
+
+void sensorhujan() {
+  String h = "Hujan";
+  String c = "Cerah";
+  int kondisi_sensor = digitalRead(hujan);
+  if (kondisi_sensor == LOW) {
+    Blynk.virtualWrite(V2,h);
+    led3.on();
+    Serial.println("Cuaca Hujan");
+    digitalWrite(buzzer, HIGH);
+  }
+  else {
+    Blynk.virtualWrite(V2,c);
+    led3.off();
+    Serial.println("Cuaca Cerah");
+    digitalWrite(buzzer, LOW);
+  }
+}
 
 void setup () {
   Serial.begin(115200);
-  pinMode (sensor_hujan, INPUT);
-  pinMode (LED, OUTPUT);
+  pinMode (hujan, INPUT);
+  pinMode (buzzer, OUTPUT);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  timer.setInterval(1000L, sensorhujan);
 }
 
 void loop() {
-  int kondisi_sensor = digitalRead(sensor_hujan);
-  if (kondisi_sensor == LOW) {
-    Serial.println("Cuaca Hujan");
-    digitalWrite(LED, HIGH);
-  }
-  else {
-    Serial.println("Cuaca Cerah");
-    digitalWrite(LED, LOW);
-  }
-  delay (1000);
+  Blynk.run();
+  timer.run();
 }
