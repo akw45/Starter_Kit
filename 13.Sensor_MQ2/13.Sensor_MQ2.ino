261e3f119545bae490fabb68df9edd08463419ce
Arya Kamandanu <1829141024@student.unm.ac.id>
2023-04-10 12:33:30 +0800

    starter kit 2.3

--- 13.Sensor_MQ2/13.Sensor_MQ2.ino
+++ 13.Sensor_MQ2/13.Sensor_MQ2.ino
@@ -1,18 +1,25 @@
-int Buzzer = 4;        
-int Gas_analog = A0;   
-int Gas_digital = 5;  
+#define BLYNK_PRINT Serial
 
+#define BLYNK_AUTH_TOKEN            "oRP17_S_QER-8ZwKHleG1ltf-5W5KHIY"
 
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(Buzzer, OUTPUT);      
-  pinMode(Gas_digital, INPUT);
-}
+#define pinA A0
+#define pinD 4
+#define buzzer 5
 
-void loop() {
-  int gassensorAnalog = analogRead(Gas_analog);
-  int gassensorDigital = digitalRead(Gas_digital);
+char ssid[] = "Tukang Ngoding";
+char pass[] = "kodingeverywhere";
+
+WidgetLED led3(V3);
+
+BlynkTimer timer;
+
+void sensorMQ2() {
+  int gassensorAnalog = analogRead(pinA);
+  int gassensorDigital = digitalRead(pinD);
+
 
   Serial.print("Gas Sensor: ");
   Serial.print(gassensorAnalog);
@@ -21,15 +28,32 @@ void loop() {
   Serial.print(gassensorDigital);
   Serial.print("\t");
   Serial.print("\t");
-  
+
   if (gassensorAnalog > 900) {
+    Blynk.virtualWrite(V5, "Gas Terdeteksi");
     Serial.println("Gas");
-    digitalWrite (Buzzer, HIGH) ; //send tone
+    led3.on();
+    digitalWrite (buzzer, HIGH) ; //send tone
     delay(1000);
-    digitalWrite (Buzzer, LOW) ;  //no tone
+    digitalWrite (buzzer, LOW) ;  //no tone
   }
   else {
+    Blynk.virtualWrite(V5, "Gas Tidak Terdeteksi");
+    led3.off();
     Serial.println("No Gas");
   }
   delay(100);
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pinA, INPUT); //Declare pin A0 as input
+  pinMode(buzzer, OUTPUT);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  timer.setInterval(500L, sensorMQ2);
+}
+
+void loop() {
+  Blynk.run();
+  timer.run();
 }
